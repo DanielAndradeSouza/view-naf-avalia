98bbba0b8da5ab3ba5d60d5e80fbc1a59d3f8548
Daniel <dansouza359157@gmail.com>
2025-09-15 14:24:31 -0300

    Fiz o esqueleto da pagina do Form.

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import "../index.css";
+import "../styles/Form.css"
 import questions from "./questions.json"
 //São um total de 24 questões, muitas sendo radio questions, ou seja com valores predefinidos mas com apenas uma resposta
 function Form(){
-
+    const totalQuestions = questions.length;
+    const [answerState,setAnswerState] = useState();
+    const [pageState,setPageState] = useState(0);
+    const nextPage = () => setPageState(prev => prev+1);
+    const prevPage = () => setPageState(prev => prev-1);
+    const q = questions[pageState];
     return (
         <div className="container">
             <h1>Formulário</h1>
-            {questions.map((q,indexQ) => (
-                <div key={indexQ}>
-                <p>{q.question}</p>
-                
-                {q.answers.map((answer,i) => (
-                    <label key={i}>
-                        <input type={q.type} key={q.id}
-                        />
-                        {answer}</label>
-                ))}
-                </div>
+            <p>{q.question}</p>
+            <div className="questions">
+            {q.answers.map((answer,index) => (
+                <label key={index}>
+                    <input type={q.type} name="" id="" />
+                    {answer}
+                </label>
             ))}
-            
+            </div>
+            <button>Questão Anterior</button> <button>Próxima Questão</button>
         </div>
     );
 }
