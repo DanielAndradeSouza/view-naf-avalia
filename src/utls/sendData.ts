4e87c128db54ee13faa1336814e8267f556c5f7f
Daniel <dansouza359157@gmail.com>
2025-09-22 16:55:16 -0300

    sendData

--- src/utls/sendData.ts
+++ src/utls/sendData.ts
@@ -1,4 +1,9 @@
 function sendData(request:any){
-    
+    const response = fetch("/Form",{
+        method:"POST",
+        body:JSON.stringify(request),
+        headers: {"Content-Type":"application/json"}
+    });
+    console.log(response);
 }
 export default sendData;
