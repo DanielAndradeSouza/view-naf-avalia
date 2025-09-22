9ce1ac81278be4b9591805ffd4ebf8f66e2e151d
Daniel <dansouza359157@gmail.com>
2025-09-22 16:58:29 -0300

    try catch

--- src/utls/sendData.ts
+++ src/utls/sendData.ts
@@ -1,9 +1,13 @@
 function sendData(request:any){
+    try{
     const response = fetch("/Form",{
         method:"POST",
         body:JSON.stringify(request),
         headers: {"Content-Type":"application/json"}
     });
     console.log(response);
+    }catch(error){
+        console.log("Erro encontrado: ", error)
+    }
 }
 export default sendData;
