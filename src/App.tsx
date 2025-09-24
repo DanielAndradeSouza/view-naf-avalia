037e9e982884b859000ea69e7a5806661ee0c36b
Daniel <dansouza359157@gmail.com>
2025-09-23 23:08:20 -0300

    renderizei ambos

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Form from './pages/Form';
 import End from './pages/End';
+import ModifyForm from './pages/ModifyForm';
+import CreateQuestion from './pages/CreateQuestion';
 
 function App(){
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/modifyForm'></Route>
-                <Route path='/createQuestion'></Route>
+                <Route path='/modifyForm' element={<ModifyForm />}></Route>
+                <Route path='/createQuestion' element={<CreateQuestion/>} ></Route>
                 <Route path='/form' element={<Form />} />
                 <Route path='/end' element={<End/>}></Route>
             </Routes>
