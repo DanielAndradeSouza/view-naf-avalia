02734296f993f9b4de54f37e584eeba10f590929
Daniel <dansouza359157@gmail.com>
2025-09-26 18:53:03 -0300

    precisa melhorar o select e input text do index.css

--- src/pages/CreateQuestion.tsx
+++ src/pages/CreateQuestion.tsx
@@ -1,5 +1,4 @@
 import "../index.css";
-import "../styles/Form.css"
 import LogoImg from "../component/logo_img";
 import { useState } from "react";
 function CreateQuestion(){
@@ -11,11 +10,13 @@ function CreateQuestion(){
             <LogoImg></LogoImg>
             <div className="conteiner">
                 <div className="questions">
+                    <label htmlFor="text">Cabeçalho</label>
                 <input type="text" onChange={(e) => setText(e.target.value)}/>
                 <select name="type" id="type">
                     <option value="checkbox">Checkbox</option>
                     <option value="radio">Radio</option>
                 </select>
+                <button>Registrar</button>
                 </div>
             </div>
         </div>
