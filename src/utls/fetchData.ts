83503c39b00dc91a6a9b7bb8b70c9e4cf6c134ca
Daniel <dansouza359157@gmail.com>
2025-09-26 15:05:27 -0300

    teste no fetch data

--- src/utls/fetchData.ts
+++ src/utls/fetchData.ts
@@ -2,11 +2,11 @@
 export default async function fetchData(url: string) {
   try {
     const response = await fetch(`http://localhost:8000/${url}`);
-    console.log(response.body);
     if (!response.ok) {
       throw new Error(`Erro ao buscar dados: ${response.status}`);
     }
     const data = await response.json();
+    console.log(data);
     return data; 
   } catch (error) {
     console.error(error);
